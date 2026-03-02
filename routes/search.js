e14737151127104f57614d35fc64c75152523a62
lollekacc <lolekacc@gmail.com>
2026-03-02 11:28:38 +0100

    page layout

--- routes/search.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { searchPlans } from "../controllers/searchController.js";
-
-const router = express.Router();
-
-router.post("/", searchPlans);   // using POST since your controller expects req.body
-
-export default router;
